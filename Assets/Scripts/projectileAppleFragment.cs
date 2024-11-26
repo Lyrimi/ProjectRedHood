e5663d102853e8bef67c065ae1343761f926a836
1rasmusrune1 <1rasmusrune1@gmail.com>
2024-11-26 09:11:04 +0100

    Made appleFragment shrink over time
    AppleFragments now shrinks and disappears over time instead of lingering.

--- Assets/Scripts/projectileAppleFragment.cs
+++ Assets/Scripts/projectileAppleFragment.cs
@@ -2,17 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class projectileAppleFragment : MonoBehaviour
-{
+public class projectileAppleFragment : MonoBehaviour {
+    public float shrinkTimeMin;
+    public float shrinkTimeMax;
+    public float shrinkBeginTime;
+    float shrinkStartTime;
+    float shrinkTime;
+    Vector2 originalScale;
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
+        shrinkStartTime = Time.time+shrinkBeginTime;
+        shrinkTime = Random.Range(shrinkTimeMin, shrinkTimeMax);
+        originalScale = transform.localScale;
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+        float time = Time.time - shrinkStartTime;
+        if (time >= 0) {
+            if (time >= shrinkTime) {
+                Destroy(gameObject);
+            } else {
+                transform.localScale = originalScale * (1 - time / shrinkTime);
+            }
+        }
     }
 }
