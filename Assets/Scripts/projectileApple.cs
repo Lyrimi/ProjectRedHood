20cbe1d751a7c0d47749b741e22d761562020328
1rasmusrune1 <1rasmusrune1@gmail.com>
2024-12-06 20:04:19 +0100

    Fixed apple projectile sometimes throwing an error upon breaking
    The apple projectile would sometimes complain about no contacts when breaking.
    Also, the apple now disables its collision and physics immediately upon breaking.

--- Assets/Scripts/projectileApple.cs
+++ Assets/Scripts/projectileApple.cs
@@ -32,21 +32,22 @@ public class apple : projectileBase
                 /*Vector2 surfaceNormal = collision.GetContact(0).normal;
                 float deflectionAngle = 2*Mathf.Atan2(surfaceNormal.y, surfaceNormal.x)-Mathf.Atan2(rb.velocity.y, rb.velocity.x)+Mathf.PI;
                 Vector2 deflection = new Vector2(Mathf.Cos(deflectionAngle), Mathf.Sin(deflectionAngle))*rb.velocity.magnitude;*/
-                Vector2 deflection = collision.GetContact(0).normal*rb.velocity.magnitude;
-                //Collider2D[] colliders = new Collider2D[fragmentCount];
+                Vector2 deflection;
+                {
+                    if (collision.contactCount != 0) {
+                        deflection = collision.GetContact(0).normal*rb.velocity.magnitude;
+                    } else {
+                        deflection = Vector2.zero;
+                    }
+                }
                 for (int i = 0; i < fragmentCount; i++) {
                     GameObject fragment = Instantiate(fragmentObject, transform.position, Quaternion.identity);
                     Vector2 vel = deflection*deflectionMultiplier*(1+Random.Range(-deflectionMultiplierRange, deflectionMultiplierRange));
                     float angle = Random.Range(-180, 180);
                     float scatterSpeed = Random.Range(minScatterSpeed, maxScatterSpeed);
                     fragment.GetComponent<Rigidbody2D>().velocity = new Vector2(Mathf.Cos(angle)*scatterSpeed, Mathf.Sin(angle)*scatterSpeed)+vel;
-                    /*colliders[i] = (Collider2D) fragment.GetComponent("Collider2D");
-                    for (int o = 0; o < i; o++) {
-                        Physics2D.IgnoreCollision(colliders[i], colliders[o]);
-                    }*/
                 }
             }
-            Destroy(spriteObject);
             Destroy(gameObject);
         }
     }
@@ -54,6 +55,8 @@ public class apple : projectileBase
     void OnCollisionEnter2D(Collision2D collision) {
         if (this.collision == null) {
             this.collision = collision;
+            GetComponent<Collider2D>().enabled = false;
+            rb.simulated = false;
         }
     }
 }
